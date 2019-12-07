54d53f7b6674be474ab778cd32ec18e78cb688ea
parocuant <56595662+parocuant@users.noreply.github.com>
2019-12-06 23:21:20 -0300

    Update settings.py

--- lineup/settings.py
+++ lineup/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATIC_DIR = os.path.join(BASE_DIR, '/lineup/Apps/gestionEntradas/static')
 
 STATIC_URL = '/static/'
 
