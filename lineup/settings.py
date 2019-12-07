3aca742533129fb6abc603c6924583d1d1b74d0a
parocuant <56595662+parocuant@users.noreply.github.com>
2019-12-07 00:33:03 -0300

    Update settings.py

--- lineup/settings.py
+++ lineup/settings.py
@@ -122,8 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = "/home/procuant/lineup/Apps/gestionEntradas/static"
 
-STATIC_URL= "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATICFILES_DIRS = [STATIC_DIR, ]
